fff4767fa548675a9ee6e03a096e97e6d7059569
gordonlzy <gordonlzy123@gmail.com>
2022-03-10 11:24:03 +0800

    Refactor weather api as service

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { WeatherApiService } from '../weather-api.service';
 
 @Component({
   selector: 'app-panel',
@@ -27,47 +25,31 @@ export class PanelComponent implements OnInit {
     }
   }
 
-  handleError(error: HttpErrorResponse) {
-    return throwError(error.status + " " + error.statusText);
-  }
-
   changeBackground(weather:string) {
     if (weather == "Clear") {
       this.background = {'sunny-background': true, 'clouds-background': false, 'rainy-background': false}
-    } else if (weather == "Clouds") {
+    } else if (weather == "Clouds" || weather == "Haze") {
       this.background = {'sunny-background': false, 'clouds-background': true, 'rainy-background': false}
     } else if (weather == "Rain") {
       this.background = {'sunny-background': false, 'clouds-background': false, 'rainy-background': true}
     }
     return null;
-}
+  }
 
-  fetchData() {
-    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${this.panelText}&appid=7d7bb0af43748b88ab08a9945fa1d241`;
-    const options = {
-      observe: 'body',
-      responseType: 'json'
-    }
-    const res = this.http.get<any>(endpoint)
-    
-    res
-      .pipe(catchError(err => {
-        this.hasError = true;
-        this.isReadyToDisplayText = false;
-        this.error = err.error.message;
-        return throwError(err.status);
-      }))
+  async handleInput(e: Event) {
+    this.isWaitingFirstInput = false;
+    this.weatherAPIService
+      .fetchData(this.panelText)
       .subscribe(data => {
         this.error = '';
         this.changeBackground(data["weather"][0]["main"]);
         this.hasError = false;
         this.isReadyToDisplayText = true;
+      }, err => {
+        this.hasError = true;
+        this.isReadyToDisplayText = false;
+        this.error = err.error.message;
       });
-  }
-
-  async handleInput(e: Event) {
-    this.isWaitingFirstInput = false;
-    this.fetchData()
   }
 
   handleEdit(e: Event) {
@@ -76,7 +58,7 @@ export class PanelComponent implements OnInit {
     this.isReadyToDisplayText = false;
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private weatherAPIService: WeatherApiService) { }
 
   ngOnInit(): void {
   }
---
+++ src/app/weather-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherApiService {
+
+  constructor(private http: HttpClient) { }
+
+  fetchData(panelText:string) {
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${panelText}&appid=7d7bb0af43748b88ab08a9945fa1d241`;
+    const options = {
+      observe: 'body',
+      responseType: 'json'
+    }
+    return this.http.get<any>(endpoint)
+      .pipe(catchError(err => throwError(err)));
+  }
+}
